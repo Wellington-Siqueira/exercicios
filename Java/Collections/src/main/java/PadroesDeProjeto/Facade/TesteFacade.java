8b131922df39af42a2d01123cb8917ba01c144cd
Wellington-Siqueira <wellingtonsiqueira333@gmail.com>
2022-05-09 22:07:20 -0300

    facade

---
+++ Java/Collections/src/main/java/PadroesDeProjeto/Facade/TesteFacade.java
@@ -0,0 +1,8 @@
+package PadroesDeProjeto.Facade;
+
+public class TesteFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Jose", "11335577889");
+    }
+}
